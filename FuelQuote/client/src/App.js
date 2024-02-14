39fed4d22864fb9dd40fc7d9952efcd0a396bf64
Bryant Huynh <bryant.huynh03@gmail.com>
2024-02-13 23:52:51 -0600

    Created ClientProfile

--- FuelQuote/client/src/App.js
+++ FuelQuote/client/src/App.js
@@ -1,12 +1,17 @@
 
+import { Routes, Route, Router } from 'react-router-dom';
 import './App.css';
+import ClientProfile from './Components/ClientProfile/ClientProfile';
 import Loginpage from './Components/loginpage';
 
 function App() {
   return (
-    <div className="App">
-      <Loginpage />
-    </div>
+      <div className="App">
+        <Loginpage />
+        <Routes>
+          <Route path = "/ClientProfile" element = {<ClientProfile />} /> 
+        </Routes>
+      </div>
   );
 }
 
---
+++ FuelQuote/client/src/Components/ClientProfile/ClientProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClientProfile = () => {
+
+    return (
+        <p>CLIENT PROFILE</p>
+    );
+
+};
+
+export default ClientProfile;
