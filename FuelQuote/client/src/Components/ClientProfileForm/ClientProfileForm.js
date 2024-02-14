0db86d5aaedc7c1f95b5727f17c60ccda242f5f5
Bryant Huynh <bryant.huynh03@gmail.com>
2024-02-14 12:39:51 -0600

    Created Full Name field

--- FuelQuote/client/src/Components/ClientProfileForm/ClientProfileForm.js
+++ FuelQuote/client/src/Components/ClientProfileForm/ClientProfileForm.js
@@ -3,8 +3,15 @@ import React from "react";
 const ClientProfileForm = () => {
 
     return (
-        <div>
+        <div class = "wrapper">
             <p>CLIENT PROFILE</p>
+            <div class = "inputfields">
+                <div className = "input-box">
+                    <div class = "input-row-1">
+                        <input type = "fullName" placeholder = 'Full Name' required minLength = "1" maxLength = "50"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
