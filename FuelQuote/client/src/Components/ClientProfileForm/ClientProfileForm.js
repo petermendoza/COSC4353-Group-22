240c344bf2f01df50171c4eb52e1d4d0c7e4ca7e
petermendoza <peterjohnm2002@gmail.com>
2024-02-21 20:30:25 -0600

    feat: add profile page

--- FuelQuote/client/src/Components/ClientProfileForm/ClientProfileForm.js
+++ FuelQuote/client/src/Components/ClientProfileForm/ClientProfileForm.js
@@ -4,10 +4,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ClientProfileForm = () => {
+    const fullName = 'Anita M. Wynn';
+    const address1 = 'Main st 123';
+    const address2 = 'N/A';
+    const city = 'Houston';
+    const zipcode = '77204'
+    const state = 'TX'
 
     return (
-        <div class = "wrapper">
-            <p>WILL FINISH TOMORROW</p>
+        <div class = "wrapper-large" id="wrapper-profile">
+            <h1>Profile</h1>
+            <div class="profile-row">
+                <p>Full Name:</p>
+                <p>{fullName}</p>
+            </div>
+            <div class="profile-row">
+                <p>Address 1:</p>
+                <p>{address1}</p>
+            </div>
+            <div class="profile-row">
+                <p>Address 2:</p>
+                <p>{address2}</p>
+            </div>
+            <div class="profile-row">
+                <p>City:</p>
+                <p>{city}</p>
+            </div>
+            <div class="profile-row">
+                <p>Zipcode:</p>
+                <p>{zipcode}</p>
+            </div>
+            <div class="profile-row">
+                <p>State:</p>
+                <p>{state}</p>
+            </div>
+
             <Link to = "/Profile/Edit">
                 <button>Edit Profile</button>
             </Link>
