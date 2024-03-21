f6222f1c41e092b056efb0bf20a3c1e115b72f4e
Bryant Huynh <bryant.huynh03@gmail.com>
2024-03-21 02:09:17 -0500

    Feat: History fetch working

--- FuelQuote/client/src/Components/FuelQuoteHistory.js
+++ FuelQuote/client/src/Components/FuelQuoteHistory.js
@@ -1,44 +1,12 @@
 import '../Style/Components/FuelQuoteHistory.css';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
-const historyData = [
-    {
-        clientID: 202020,
-        quoteID: 123456,
-        dateRequested: 'Feburary 19, 2024',
-        gallonsRequested: 100,
-        deliveryAddress: 'Address of Delivery',
-        deliveryDate: 'March 20, 2024',
-        suggestedPrice: 20.24,
-        totalAmountDue: 1000.99
-    },
-    {
-        clientID: 101010,
-        quoteID: 111111,
-        dateRequested: 'Feburary 7, 2024',
-        gallonsRequested: 50,
-        deliveryAddress: 'Address of Delivery',
-        deliveryDate: 'May 20, 2024',
-        suggestedPrice: 30.68,
-        totalAmountDue: 2984.99 
-    },
-    {
-        clientID: 303030,
-        quoteID: 333333,
-        dateRequested: 'March 7, 2024',
-        gallonsRequested: 50,
-        deliveryAddress: 'Address of Delivery',
-        deliveryDate: 'August 20, 2024',
-        suggestedPrice: 30.68,
-        totalAmountDue: 2984.99 
-    }
-]
-
 const FuelQuoteHistory = () => {
 
+    const [history,setHistory] = useState(null)
     const [selected,setSelected] = useState(null)
 
     const toggle = (i) => {
@@ -49,12 +17,25 @@ const FuelQuoteHistory = () => {
         setSelected(i)
     }
 
+    useEffect( () => {
+        const fetchHistory = async () => {
+            const response = await fetch('/api/fuelQuote')
+            const json = await response.json()
+
+            if (response.ok) {
+                setHistory(json)
+            }
+        } 
+
+        fetchHistory()
+    }, [])
+
     return (
         <div className = "historywrapper">
             
             <div className = "accordian">
-
-                {historyData.map((quote, i) => (
+                
+                {history.map((quote, i) => (
                     
                     <div className = "quote">
 
--- FuelQuote/server/routes/fuelQuote.js
+++ FuelQuote/server/routes/fuelQuote.js
@@ -4,7 +4,27 @@ const router = express.Router()
 
 // Get all Fuel Quotes
 router.get('/', (req, res) => {
-    res.json({ msg: 'Get all Fuel Quotes'});
+    res.json([{
+        "clientID": 202020,
+        "quoteID": 123456,
+        "dateRequested": "Feburary 19, 2024",
+        "gallonsRequested": 100,
+        "deliveryAddress": "Address of Delivery",
+        "deliveryDate": "March 20, 2024",
+        "suggestedPrice": 20.24,
+        "totalAmountDue": 1000.99
+    },
+    {
+        "clientID": 101010,
+        "quoteID": 111111,
+        "dateRequested": "Feburary 7, 2024",
+        "gallonsRequested": 50,
+        "deliveryAddress": "Address of Delivery",
+        "deliveryDate": "May 20, 2024",
+        "suggestedPrice": 30.68,
+        "totalAmountDue": 2984.99 
+    }
+    ]);
 })
 
 // Post new Fuel Quote
