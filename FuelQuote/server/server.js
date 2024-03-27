414766e95683c172ab4a440ae3f82c151b6f5802
rphong <richard.phong424@gmail.com>
2024-03-27 13:07:37 -0500

    chore: clean up server.js

--- FuelQuote/server/server.js
+++ FuelQuote/server/server.js
@@ -1,55 +1,29 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 const app = express();
 
-const cors = require("cors");
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(errorHandler);
 
-//imported routes for login & signUp here
-const userRoutes = require('./routes/user')
+// Log requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-// Importing fuel quote routes
-const fuelQuoteRoutes = require('./routes/fuelQuote')
+// Routes
+app.use('/api/profile', require('./routes/profileRoutes'));
+app.use('/api/user', require('./routes/user'));
+app.use('/api/fuelQuote', require('./routes/fuelQuote'));
+app.use('/api/userLogin',require('./routes/login'));
 
-//Importing login routes 
-const loginRoutes = require('./routes/login')
-
+// Port
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-
-app.use(express.json());
-
-// May not need
-app.use(require("./routes/record"));
-
-app.use('/api/profile', require('./routes/profileRoutes'))
-
-
-// Error Handler
-app.use(errorHandler)
-// Get MongoDB driver connection
-const dbo = require("./database/conn");
- 
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
-//resgister routes for login & signUp here
-app.use('/api/user', userRoutes)
-
-// Registering routes for Fuel Quotes
-app.use('/api/fuelQuote', fuelQuoteRoutes)
-
-//Registering routes for Login 
-app.use('/api/userLogin',loginRoutes)
-
 app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
   console.log(`Server is running on port: ${port}`);
 });
